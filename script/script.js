a520dcb68d2fc644591d8703d65da62a208eea2e
KratoDNS <gena.systerov@yandex.ru>
2020-10-09 18:21:02 +0300

    fix: add semilicon && output console

--- script/script.js
+++ script/script.js
@@ -1,17 +1,16 @@
-let money=20
-let income=1000
-let addExpenses=200
-let deposit= 40
-let mission=45
-let period=90
+let money= 20;
+let income= 1000;
+let addExpenses= 200;
+let deposit= 40;
+let mission= 45;
+let period= 90;
 
-alert("Heeeeeeeyyyyyoooo!!!");;
+alert("Heeeeeeeyyyyyoooo!!!");
 
-
-console.log(money)
-console.log(income)
-console.log(addExpenses)
-console.log(deposit)
-console.log(mission)
-console.log(period)
-console.log(2102200);
+console.log(money,
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period,
+    2102200);
