b93590c4f78220cc17c255f566b49800d71ebec1
KratoDNS <gena.systerov@yandex.ru>
2020-10-09 18:18:08 +0300

    init

---
+++ script/script.js
@@ -0,0 +1,17 @@
+let money=20
+let income=1000
+let addExpenses=200
+let deposit= 40
+let mission=45
+let period=90
+
+alert("Heeeeeeeyyyyyoooo!!!");;
+
+
+console.log(money)
+console.log(income)
+console.log(addExpenses)
+console.log(deposit)
+console.log(mission)
+console.log(period)
+console.log(2102200);
