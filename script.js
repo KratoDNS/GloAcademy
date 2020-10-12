8d960bfbe0fe127c81517b65eb67935a97a2830b
KratoDNS <gena.systerov@yandex.ru>
2020-10-12 15:41:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let money=60000
+let income= 'создание сайтов'
+var addExpenses='Интернет, Коммуналка, Табак)))'
+let deposit= Boolean(40)
+let mission=50000
+let period=4
+let budgetDay= money/30
+
+
+alert("Heeeeeeeyyyyyoooo!!!");
+
+
+console.log(typeof money)
+console.log(typeof income)
+console.log(addExpenses.length);
+console.log(typeof deposit);
+console.log('цель заработать '+ mission + ' рублей');
+console.log('период равен '+ period +' месяцев');
+console.log(addExpenses.toLowerCase().split());
+console.log(2102200);
+console.log(addExpenses.split(' ', 3));
+console.log(budgetDay)
