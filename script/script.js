6f732cbbea753fd791a4b4589b9dc13703650219
KratoDNS <gena.systerov@yandex.ru>
2020-10-12 18:32:02 +0300

    первый вариант

--- script/script.js
+++ script/script.js
@@ -1,13 +1,11 @@
 let money=60000
 let income= 'создание сайтов'
-var addExpenses='Интернет, Коммуналка, Табак)))'
-let deposit= Boolean(40)
+let addExpenses='Интернет, Коммуналка, Табак)))'
+let deposit= true
 let mission=50000
 let period=4
 let budgetDay= money/30
 
-
-alert("Heeeeeeeyyyyyoooo!!!");
 
 
 console.log(typeof money)
@@ -17,6 +15,4 @@ console.log(typeof deposit);
 console.log('цель заработать '+ mission + ' рублей');
 console.log('период равен '+ period +' месяцев');
 console.log(addExpenses.toLowerCase().split());
-console.log(2102200);
-console.log(addExpenses.split(' ', 3));
 console.log(budgetDay)
